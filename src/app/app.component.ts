36dddcbddd960610a7bd7bf80e0c38480181aa5a
elp4nsho <l.m_d.m@hotmail.com>
2021-05-20 10:58:22 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,163 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import {environment} from "../environments/environment";
+
+export interface Item {
+  detalle: String,
+  valor: number
+}
+
+export interface Cliente {
+  nombre: String,
+  direccion: String,
+  telefono: String,
+  objetos: Array<Item>,
+}
+
+export interface Boleta {
+  fecha: String,
+  cliente: Cliente,
+  metodoDePago: String,
+  metodoDeEnvio: String,
+  total: String,
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  VIDEOGAMES = [
-    {
-      id: 1,
-      name: 'Animal Crossing',
-      platform: 'Nintendo Switch',
-      reference: '1-770-736-8031',
-    },
-    {
-      id: 2,
-      name: 'The Legend of Zelda: Ocarina of Time CV',
-      platform: 'Wii U',
-      reference: '1-770-736-2323',
-    },
-    {
-      id: 3,
-      name: 'Metal Gear Solid',
-      platform: 'Playstation (PSX)',
-      reference: '1-4564-736-334',
-    },
-    {
-      id: 4,
-      name: 'ShenMue',
-      platform: 'Sega Dreamcast',
-      reference: '3-770-736-4532',
-    },
-    {
-      id: 5,
-      name: 'Rise of the Tomb Raider',
-      platform: 'Playstation 4',
-      reference: '1-324-736-3245',
-    },
-    {
-      id: 6,
-      name: 'Resident Evil 2',
-      platform: 'Playstation',
-      reference: '1-123-3336-4321',
-    },
-  ];
+
+  total = 0;
+
+  cliente: Cliente = {
+    direccion: "",
+    nombre: "",
+    telefono: "",
+    objetos: []
+  }
+
+  itemVacio: Item = {
+    detalle: "",
+    valor: 0
+  };
+
+  metodoDePago = ""
+  metodoDeEnvio = ""
+
+  items: Array<Item> = [];
+
+  constructor() {
+    this.sumarItem()
+  }
+
+
+  sumarItem() {
+    let newItem: Item = {
+      detalle: "",
+      valor: undefined
+    }
+    this.items.push(newItem)
+  }
+
+  sumarTotal() {
+    let total = 0;
+    for (let it of this.items) {
+      try {
+        total = +it.valor + +total
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    this.total = total;
+  }
+
+  borrar(i) {
+    let nuevaLista = []
+    for (let it = 0; it < this.items.length; it++) {
+      if (it == i) {
+
+      } else {
+        nuevaLista.push(this.items[it])
+      }
+    }
+    this.items = nuevaLista
+
+  }
+
+
   // tslint:disable-next-line:typedef
   downloadPDF() {
-    const DATA = document.getElementById('htmlData');
+
+
+    console.log("los datoooos a pdfiar");
+    this.cliente.objetos = this.items;
+    let boleta: Boleta = {
+      cliente: this.cliente,
+      fecha: new Date().toString(),
+      total: this.total.toString(),
+      metodoDeEnvio: this.metodoDeEnvio,
+      metodoDePago: this.metodoDePago
+    }
+    console.log(boleta);
     const doc = new jsPDF('p', 'pt', 'a4');
-    const options = {
-      background: 'white',
-      scale: 3
-    };
-    html2canvas(DATA, options).then((canvas) => {
 
-      const img = canvas.toDataURL('image/PNG');
+    var imgData = environment.foto
 
-      // Add image Canvas to PDF
-      const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
-    }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
-    });
+
+    doc.setFontSize(25)
+    doc.addImage(imgData, 'JPEG', 15, 40, 500, 700)
+
+    doc.text(35, 55, "Nombre: ")
+    doc.text(170, 55, this.cliente.nombre)
+    doc.text(35, 95, "Direccion")
+    doc.text(170, 95, this.cliente.direccion)
+    doc.text(35, 130, "Telefono")
+    doc.text(170, 130, this.cliente.telefono)
+
+    doc.setFontSize(20)
+    doc.text(130, 160 ,"Detalle")
+    doc.text(330, 160 ,"Valor")
+
+    for (let it = 0; it < this.cliente.objetos.length; it++) {
+      doc.text(130, 190+it*50, this.cliente.objetos[it].detalle + this.cliente.objetos[it].valor.toString())
+      doc.text(330, 190+it*50, this.cliente.objetos[it].valor.toString())
+    }
+    doc.text(35, (150+this.cliente.objetos.length*30)+50, "TOTAL: "+boleta.total )
+    doc.text(35, (150+this.cliente.objetos.length*30)+100, "Metodo de pago:")
+    doc.text(135, (150+this.cliente.objetos.length*30)+100, this.metodoDePago )
+    doc.text(35, (150+this.cliente.objetos.length*30)+150, "Metodo de envio:")
+    doc.text(135, (150+this.cliente.objetos.length*30)+150, this.metodoDeEnvio )
+
+
+
+    doc.save("boleta" + this.cliente.nombre + this.cliente.telefono + ".pdf")
+
+    /*  const DATA = document.getElementById('htmlData');
+      const doc = new jsPDF('p', 'pt', 'a4');
+      const options = {
+        background: 'white',
+        scale: 3
+      };
+      html2canvas(DATA, options).then((canvas) => {
+
+        const img = canvas.toDataURL('image/PNG');
+
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        return doc;
+      }).then((docResult) => {
+
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      });*/
   }
 }
---
+++ src/app/imagen.ts
@@ -0,0 +1,3 @@
+let datos = {
+  "foto":"aa"
+}
